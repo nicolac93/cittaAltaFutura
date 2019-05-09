64b1b201594b4de91f03c0ed5643f0b85ccf98f7
Black4Key <kevin.got10@gmail.com>
2019-05-09 12:12:50 +0200

    riordino file, template pagina base con login

--- calendario.php
+++ calendario.php
@@ -62,66 +62,10 @@ include('session.php');
           </div>
         </nav>
     
-        <header>
-            <div id="carouselExampleIndicators" class="carousel slide">
-                <div class="carousel-inner" role="listbox">
-                  <div class="carousel-item active" style="background-color:black;">
-                    <video autoplay muted loop style="height:100%;width:100%;opacity:0.6;">
-                        <source src="img/BergamoPublicSpace_corto.mp4" type="video/mp4">
-                    </video>
-                    <div class="centered_btn">
-                        <a href="survey.html" class="btn_partecipa">Partecipa!</a>
-                    </div>
-                  </div>
-                </div>
-            </div>
-            
-        </header>
-
-        <section id="services" style="margin-top:8px;">
-            <div class="container">
-            <div class="row">
-                    <div class="col-lg-12" style="text-align: center;">
-                        <h1 class="section-heading">CITTA ALTA PLURALE</h1>
-                        <h3>Processo partecipativo Tripla elica per la revisione del Piano Particolareggiato di Città Alta e Borgo Canale (PPRCA)</h3>
-                        <hr class="primary">
-                        <p> Un processo molto importante per Città Alta che mira ad ottenere un ampio coinvolgimento degli abitanti per creare le basi per la revisione del Piano Particolareggiato per Città Alta e Borgo Canale (PPRCA): un percorso che il Comune e l’Università di Bergamo intraprendono insieme, con il coordinamento scientifico del Centro Studi sul Territorio diretto dalla Prof. Emanuela Casti. 
-Il Comune ha incaricato il team di ricerca del CST nella creazione di un processo partecipativo con lo scopo di sottoporre agli abitanti intesi come residenti e city-users (pendolari, commercianti, studenti, visitatori e turisti) una revisione del Piano Particolareggiato di Città Alta volto a dare una risposta ai loro bisogni e al contempo promuovere una valorizzazione delle potenzialità territoriali del centro storico visto come polo di una città reticolare e multicentrica.
-Il processo partecipativo richiama un modello di sviluppo denominato “Tripla Elica” – basato sull’interazione tra Università, Pubblica Amministrazione e Settore privato – che, sperimentato in molte città italiane, come Pisa, ed europee, come Oxford e Leuven, e incardinato sulla presenza dell’Università nel tessuto storico, ha dato ottimi risultati non solo per far fronte alla banalizzazione del turismo ma anche, e soprattutto, per determinare il dinamismo sociale e culturale in grado di tener vivo il senso di urbanità e di appartenenza ai luoghi richieste dai residenti.<br><br>
-Esprimi anche tu la tua opinione, per noi è importante!<br><br>
-Ti consigliamo di iniziare con la visione di questo filmato che riassume l’evoluzione di Città Alta: compirai una sorta di viaggio che ti permetterà di comprendere la stratificazione e le modificazioni che la città ha subito articolandosi in modo reticolare.<br><br>
-Per poter sognare un grande futuro per Città Alta, è fondamentale conoscere la sua storia e il suo dinamismo!
- </p>
-                    </div>
-                </div>
-            
-                <div class="row">
-                    <div class="col-lg-12" style="text-align: center;">
-                        <h2 class="section-heading">Enti Promotori</h2>
-                        <hr class="primary">
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6" style="text-align: center;">
-                        <div class="service-box">
-                            <a href="http://unibg.it"><img src="img/logoUnibg.svg" height="107" ></a>
-                            <h3></h3>
-                            <!--<h3>Università degli studi di Bergamo</h3>-->
-                        </div>
-                    </div>
-                    <div class="col-sm-6" style="text-align: center;">
-                        <div class="service-box">
-                            <img src="img/logo_comune_bg_quadrata.jpg" height="107" width="107">
-                            <h3></h3>
-                            <!--<h3>Comune di Bergamo</h3>-->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
+    
+    <!--     *********   TODO   **********     -->
+    
+    
     <!-- Footer -->
     <footer class="py-5 bg-dark">
       <div class="container">
