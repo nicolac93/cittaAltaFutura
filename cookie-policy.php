c847d91d1a8a3ec9d5fa8a25ef71eb8b53556286
Hideomi Koishi <demi@obliquid.it>
2019-06-26 13:13:19 +0200

    aggiunto file nuovi

---
+++ cookie-policy.php
@@ -0,0 +1,92 @@
+<?php
+ini_set('display_errors', 'Off');
+//ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+include('session.php');
+?>
+<!DOCTYPE html>
+<html lang="it">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Processo partecipativo Tripla elica per la revisione del Piano Particolareggiato di Citt&agrave; Alta e Borgo Canale (PPRCA)">
+        <meta name="author" content="CST - DiathesisLab">
+        <title>Città Alta Plurale | Cookie Policy</title>
+    <!-- Links -->
+	<?php require_once('inc/links.inc'); ?>
+    </head>
+
+    <body>
+		<header>
+			<?php require_once('inc/header.inc'); ?>
+		</header>
+    
+    
+    <!--     *********   TODO   **********     -->
+
+	<div class="container cookie-policy">
+      <div class="row">
+        <div class="col-12 text-center mt-5">
+            <h4>Città Alta Plurale</h4>
+            <h1 class="section-heading mt-0 mb-3">Cookie Policy</h1>
+            <h3>Informativa estesa sull'utilizzo dei cookie</h3>
+        </div>
+        <div class="text-left mt-5">
+            <hr class="primary">
+            <p>Questa informativa sull'utilizzo dei cookie è resa all'utente nel rispetto dell'art. 13 del Regolamento (UE) 2016/679 e in attuazione del provvedimento del Garante per la protezione dei dati personali dell'8 maggio 2014 "Individuazione delle modalità semplificate per l'informativa e l'acquisizione del consenso per l'uso dei cookie" e smi. Questo sito utilizza cookie tecnici, propri e di terze parti, per consentire la corretta navigazione e per raccogliere informazioni, in forma aggregata, sul numero degli utenti e su come questi visitano il sito stesso con l'obiettivo del miglioramento continuo del servizio. In questa pagina sono riportate informazioni sull'uso dei cookie e di tecnologie similari, su come sono utilizzati e su come gestirli.</p>
+            <h3>Definizioni</h3>
+            <p>I cookie sono brevi frammenti di testo (lettere e/o numeri) che permettono al server web di memorizzare sul client (il browser) informazioni da riutilizzare nel corso della medesima visita al sito (cookie di sessione) o in seguito, anche a distanza di giorni (cookie persistenti). I cookie vengono memorizzati, in base alle preferenze dell'utente, dal singolo browser sullo specifico dispositivo utilizzato (computer, tablet, smartphone). Tecnologie similari, come, ad esempio, web beacon, GIF trasparenti e tutte le forme di storage locale introdotte con HTML5, sono utilizzabili per raccogliere informazioni sul comportamento dell'utente e sull'utilizzo dei servizi. Nel seguito di questo documento faremo riferimento ai cookie e a tutte le tecnologie similari utilizzando semplicemente il termine "cookie".</p>
+            <h3>Tipologie di cookie</h3>
+            <p>In base alle caratteristiche e all'utilizzo dei cookie possiamo distinguere diverse categorie:</p>
+            <ul>
+				<li>Cookie tecnici:
+					<ul>
+						<li>cookie di navigazione, di funzionalità e di sessione utilizzati al solo fine di garantire il corretto funzionamento del sito;</li>
+						<li>cookie analytics utilizzati per raccogliere informazioni, in forma aggregata, sul numero degli utenti e su come questi visitano il sito stesso con l’obiettivo del miglioramento continuo delle funzionalità, dell’organizzazione dei contenuti, sul supporto delle diverse piattaforme e browser. 
+						<strong>Questo sito utilizza esclusivamente cookie di questo tipo</strong>.</li>
+					</ul>
+				</li>
+				<li>Cookie di profilazione:
+					<ul>
+						<li>cookie permanenti utilizzati per identificare (in modo anonimo e non) le preferenze dell'utente e migliorare la sua esperienza di navigazione. <strong>Questo sito non utilizza cookie di questo tipo.</strong></li>
+					</ul>
+				</li>
+            </ul>
+            <h3>Durata dei cookie</h3>
+            <p>I cookie utilizzati da questo sito terminano la propria validità al termine della sessione o dopo un tempo di inattività dell'utente fissato dal server.</p>
+            <h3>Cookie tecnici di terze parti</h3>
+            <p>Il sito si avvale del servizio Google Analytics che istanzia cookie tecnici di tipo analytics utilizzati per raccogliere informazioni, in forma aggregata, sul numero degli utenti e su come questi visitano il sito stesso. Tali informazioni sono utilizzate ai fini del miglioramento continuo delle funzionalità, dell’organizzazione dei contenuti, del supporto delle diverse piattaforme e browser.</p>
+            <h3>Gestione dei cookie</h3>
+			<p>In generale l'utente può comunque decidere se accettare o meno i cookie utilizzando le impostazioni del proprio browser. Di seguito si forniscono i riferimenti alle istruzioni per disabilitare i cookie nei browser più diffusi, rese disponibili dai relativi servizi di supporto:</p>
+			<ul>
+				<li><a href="https://support.google.com/chrome/answer/95647?hl=it" target="_blank">Chrome</a></li>
+				<li><a href="https://support.mozilla.org/it/kb/Gestione%20dei%20cookie" target="_blank">Firefox</a></li>
+				<li><a href="http://windows.microsoft.com/it-it/internet-explorer/delete-manage-cookies" target="_blank">Internet Explorer</a></li>
+				<li><a href="http://help.opera.com/Windows/12.10/it/cookies.html" target="_blank">Opera</a></li>
+				<li><a href="http://www.apple.com/it/privacy/use-of-cookies/" target="_blank">Safari</a></li>
+			</ul>
+			<p>L'impostazione può essere definita in modo specifico per i diversi siti e applicazioni web. Per disabilitare unicamente i cookie di Google Analytics è possibile procedere come da istruzioni fornite dal servizio di supporto Google:</p>
+			<li>Componente aggiuntivo del browser per la <a href="https://tools.google.com/dlpage/gaoptout?hl=it" target="_blank">disattivazione di Google Analytics</a></li>
+
+        </div>
+      </div>
+
+
+
+    </div>
+
+    
+
+    
+    
+    <!-- Footer -->
+	<?php require_once('inc/footer.inc'); ?>
+
+	<!-- Scripts -->
+	<?php require_once('inc/footerscripts.inc'); ?>
+
+    </body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,167 @@
+<?php
+ini_set('display_errors', 'Off');
+//ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+session_start();
+    // Include config file
+    require_once 'config.php';
+    require_once 'send_mail.php';
+    // Define variables and initialize with empty values
+    $username = $name = $tipo = "";
+    $username_err = $name_err = $tipo_err = "";
+    $mail_result = $mail_err = "";
+    
+    // Processing form data when form is submitted
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        // Validate username
+        if(empty(trim($_POST["username"]))){
+            $username_err = "Per favore inserisci la mail.";
+        } elseif(!(strpos(trim($_POST["username"]), '@') !== false)) {
+            $username_err = "Per favore inserisci una mail valida";
+        } else{
+            // Prepare a select statement
+            $sql = "SELECT id FROM utenti WHERE username = ?";
+
+            if($stmt = mysqli_prepare($conn, $sql)){
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "s", $param_username);
+                // Set parameters
+                $param_username = trim($_POST["username"]);
+                // Attempt to execute the prepared statement
+                if(mysqli_stmt_execute($stmt)){
+                    /* store result */
+                    mysqli_stmt_store_result($stmt);
+                    if(mysqli_stmt_num_rows($stmt) == 1){
+                        $username_err = "La mail inserita è già in uso!";
+                    } else{
+                        $username = trim($_POST["username"]);
+                    }
+                } else{
+                    echo "Qualcosa è andato storto. Per favore prova ancora fra un pò.";
+                }
+            }
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+        
+        
+        // Validate name
+        if(empty(trim($_POST['name']))){
+            $name_err = "Per favore inserisci uno username.";     
+        } else{
+            $name = trim($_POST['name']);
+        }
+        
+        // Validate name
+        if(empty(trim($_POST['tipo']))){
+            $tipo_err = "Per favore seleziona un tipo.";     
+        } else{
+            $tipo = trim($_POST['tipo']);
+        }
+
+        
+        
+        // Check input errors before inserting in database
+        if(empty($username_err) && empty($name_err) && empty($tipo_err)){
+            if(empty($mail_result)){
+                $mail_result = sendMail($username, $name, $tipo);
+                if($mail_result == "OK"){
+                    $_SESSION['username'] = $username;
+                    $_SESSION['name'] = $name;
+                    $_SESSION['tipo'] = $tipo;
+                    header("location: signup_2.php");
+                }
+                else{
+                    $mail_err = "Errore nell'invio, controllare la mail e/o la connessione";
+                }
+            }
+        }
+        
+        // Close connection
+        mysqli_close($conn);
+    }
+    ?>
+
+     
+
+    <!DOCTYPE html>
+
+    <html>
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="CST - DiathesisLab">
+		<title>Citt&agrave; Alta Plurale | Registrati</title>
+		<!-- Links -->
+		<?php require_once('inc/links.inc'); ?>
+        <style type="text/css">
+            input[type=text], input[type=password] {
+                width: 100%;
+                padding: 15px;
+                margin: 5px 0 15px 0;
+                border: none;
+                background: #f1f1f1;
+            }
+
+            input[type=text]:focus, input[type=password]:focus {
+                background-color: #ddd;
+                outline: none;
+            }
+
+        </style>
+
+    </head>
+    <body>
+		<header>
+			<?php require_once('inc/header.inc'); ?>
+		</header>
+        <div class="bg-img">
+			<div class="container">
+				<div class="row">
+					<div class="text-left my-5 mx-auto">
+						<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+							<div class="wrapper" id="phase1">
+								<h2>Registrazione</h2>
+								<p>Per favore compila i campi per creare un account.</p>
+								<div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+									<label>Mail</label>
+									<input type="text" name="username" value="<?php echo $username; ?>">
+									<span class="help-block"><?php echo $username_err; ?></span>
+								</div>
+								<div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+									<label>Username</label>
+									<input type="text" name="name" value="<?php echo $name; ?>">
+									<span class="help-block"><?php echo $name_err; ?></span>
+								</div>
+								<div class="form-group <?php echo (!empty($tipo_err)) ? 'has-error' : ''; ?>">
+									<label>Tipo</label>
+									<input type="text" name="tipo" value="<?php echo $tipo; ?>">
+									<span class="help-block"><?php echo $tipo_err; ?></span>
+								</div>
+								<div class="form-group">
+									<input type="submit" class="btn" value="Continua">
+								</div>
+								<span class="help-block"><?php echo $mail_err; ?></span>
+								<p>Hai già un profilo? <a href="login.php">Accedi qui</a>.</p>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+
+			<div class="position-absolute" style="right:0px;bottom:0px;">
+				<a href="login.php">
+					<img src="img/LogoFooter.png" style="width:216px;margin:25px;">
+				</a>
+			</div>
+            
+        </div>
+    <!-- Footer -->
+	<?php require_once('inc/footer.inc'); ?>
+	<!-- Scripts -->
+	<?php require_once('inc/footerscripts.inc'); ?>
+
+    </body>
+    </html>
